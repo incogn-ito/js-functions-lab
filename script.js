67a0aead2d2283d42addf31437171e054d2c744c
Ito Aghayere <itohan.aghayere@gmail.com>
2024-07-10 16:17:23 -0700

    Exercise 2 answers here

--- script.js
+++ script.js
@@ -36,4 +36,52 @@ const maxOfTwoNumbersGaAnswer = (x, y) => {
   console.log("Exercise 1 Result: ", maxOfTwoNumbersGaAnswer(3, 9));
 
 
-  
+  /*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+ 
+// function isAdult(age) {
+//   if (age < 18) {
+//   console.log("Minor")
+//   } else if (age >= 18) {
+//   return "Adult"
+//   }
+// }
+
+
+
+
+function isAdult(x) {
+  if (x >= 18) {
+    return "Adult"
+  } else {
+    return "Minor"
+  }
+}
+console.log("Exercise 2 Result: ", isAdult(21));
+console.log("Exercise 2 Result: ", isAdult(2));
+
+//  OR other option below if I want to use 1 return statement at the end 
+
+
+function isAdultB(x) {
+  let result 
+  if ( x >= 18) {
+    result = "Adult"
+  } else {
+    result = "Minor"
+  }
+  return result
+}
+console.log("Exercise 2 Result: ", isAdultB(21));
+console.log("Exercise 2 Result: ", isAdultB(2));
+
+
